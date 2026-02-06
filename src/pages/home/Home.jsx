181cc17e59d221e409d536e457bf9bb3923c6189
salman-khan125 <salmankhaneul@gmail.com>
2026-02-05 16:21:21 -0800

    coment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../../components/hero/Hero";
 import AboutSection from "../../components/aboutsection/AboutSection";
 import OurServices from "../../components/services/OurServices";
 import CeoMessage from "../../components/ceomessage/CeoMessage";
-import MyTeam from "../../components/myteam/MyTeam";
+import MyTeam from "../../components/myteam/myTeam";
 import ClientsSay from "../../components/clientssay/ClientsSay";
 import Background from "../../components/background/Background";
 
